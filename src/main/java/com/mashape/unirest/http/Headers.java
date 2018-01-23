bdb88d66786ae1348aec8d57aa426bd989c1822f
liujianyu <liujianyu@xjx.com>
2018-01-23 16:14:59 +0800

    edit Header file

--- src/main/java/com/mashape/unirest/http/Headers.java
+++ src/main/java/com/mashape/unirest/http/Headers.java
@@ -10,7 +10,6 @@ public class Headers extends HashMap<String, List<String>> {
 	public String getFirst(Object key) {
 		List<String> list = get(key);
 		if (list != null && list.size() > 0) {
-
 			return list.get(0);
 		}
 
