af49ad31c9b1a002e8de49511f4e46397a922985
liujianyu <liujianyu@xjx.com>
2018-01-23 16:23:38 +0800

    master-branch-1 edit 2

--- src/main/java/com/mashape/unirest/http/Headers.java
+++ src/main/java/com/mashape/unirest/http/Headers.java
@@ -8,6 +8,8 @@ public class Headers extends HashMap<String, List<String>> {
 	private static final long serialVersionUID = 71310341388734766L;
 
 	public String getFirst(Object key) {
+
+
 		List<String> list = get(key);
 		if (list != null && list.size() > 0) {
 
